e484b714a4fe39ca47705b25f1e3f6ce9d0e91d7
Vitto <freeworldiptv@gmail.com>
2015-12-03 05:50:30 -0500

    upload

--- default.py
+++ default.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------
-# Jurassic Health 
+#
 #------------------------------------------------------------
 # License: GPL (http://www.gnu.org/licenses/gpl-3.0.html)
 # Based on code from youtube addon
+#
+# 
 #------------------------------------------------------------
 
 import os
@@ -12,21 +14,21 @@ import plugintools
 import xbmc,xbmcaddon
 from addon.common.addon import Addon
 
-addonID = 'plugin.video.jurassic-health'
+addonID = 'plugin.video.jurassic.health'
 addon = Addon(addonID, sys.argv)
 local = xbmcaddon.Addon(id=addonID)
 icon = local.getAddonInfo('icon')
 
-YOUTUBE_CHANNEL_ID_1 = "Blender"
-YOUTUBE_CHANNEL_ID_2 = "robin"
-YOUTUBE_CHANNEL_ID_3 = "girls"
-YOUTUBE_CHANNEL_ID_4 = "zumba"
+YOUTUBE_CHANNEL_ID_1 = "FitnessBlender"
+YOUTUBE_CHANNEL_ID_2 = "robinforlife"
+YOUTUBE_CHANNEL_ID_3 = "myfitgirls"
+YOUTUBE_CHANNEL_ID_4 = "lovezumba"
 YOUTUBE_CHANNEL_ID_5 = "BeFit"
-YOUTUBE_CHANNEL_ID_6 = "runtastic"
-YOUTUBE_CHANNEL_ID_7 = "Perfect"
-YOUTUBE_CHANNEL_ID_8 = "popsugar"
-YOUTUBE_CHANNEL_ID_9 = "bodybuilding"
-YOUTUBE_CHANNEL_ID_10 = "superhero"
+YOUTUBE_CHANNEL_ID_6 = "runtasticFitness"
+YOUTUBE_CHANNEL_ID_7 = "Perfectfitnesstv"
+YOUTUBE_CHANNEL_ID_8 = "popsugartvfit"
+YOUTUBE_CHANNEL_ID_9 = "bodybuildandfitness"
+YOUTUBE_CHANNEL_ID_10 = "superherofitnesstv"
 YOUTUBE_CHANNEL_ID_11 = "STORYOFSHIRTLESS"
 YOUTUBE_CHANNEL_ID_12 = "TheQuestForFitness"
 YOUTUBE_CHANNEL_ID_13 = "fit"
@@ -53,77 +55,77 @@ def main_list(params):
 
     plugintools.add_item( 
         #action="", 
-        title="Blender",
+        title="Fitness Blender",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_1+"/",
         thumbnail="http://icons.iconarchive.com/icons/icons8/ios7/128/Sports-Exercise-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="robin",
+        title="robinforlife",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_2+"/",
         thumbnail="http://icons.iconarchive.com/icons/sportsbettingspot/summer-olympics/128/weightlifting-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="Girls",
+        title="My Fitness Girls",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_3+"/",
         thumbnail="http://icons.iconarchive.com/icons/sonya/swarm/128/gym-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="Zumba",
+        title="Zumba Fitness",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_4+"/",
         thumbnail="http://s3.postimg.org/lhzsp1h1f/zumba.jpg",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="Fit",
+        title="Be Fit",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_5+"/",
         thumbnail="http://icons.iconarchive.com/icons/icons-land/metro-raster-sport/128/Fitness-Hand-Grippers-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="Runtastic",
+        title="Runtastic Fitness",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_6+"/",
         thumbnail="http://icons.iconarchive.com/icons/sonya/swarm/128/Running-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="Perfect",
+        title="Perfect fitness tv",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_7+"/",
         thumbnail="http://icons.iconarchive.com/icons/sportsbettingspot/summer-olympics/128/weightlifting-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="pop sugar",
+        title="pop sugar tvfit",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_8+"/",
         thumbnail="http://icons.iconarchive.com/icons/hopstarter/3d-cartoon-vol3/128/Yahoo-Messenger-icon.png",
         folder=True )
         
     plugintools.add_item( 
         #action="", 
-        title="bodybuild",
+        title="bodybuild and fitness",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_9+"/",
         thumbnail="http://icons.iconarchive.com/icons/sonya/swarm/128/Running-icon.png",
         folder=True )
 
     plugintools.add_item( 
         #action="", 
-        title="super hero",
+        title="super hero fitness tv",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_10+"/",
         thumbnail="http://icons.iconarchive.com/icons/icons8/ios7/128/Sports-Exercise-icon.png",
         folder=True )                
 
     plugintools.add_item( 
         #action="", 
-        title="Fit Media",
+        title="Fit Media Channel",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_11+"/",
         thumbnail="http://icons.iconarchive.com/icons/iconsmind/outline/128/Bodybuilding-icon.png",
         folder=True )    
@@ -137,14 +139,14 @@ def main_list(params):
 
     plugintools.add_item( 
         #action="", 
-        title="FiT – Global",
+        title="FiT – Global Fitness Network",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_13+"/",
         thumbnail="http://icons.iconarchive.com/icons/iconsmind/outline/128/weight-Lift-icon.png",
         folder=True )  
 
     plugintools.add_item( 
         #action="", 
-        title="Shaun",
+        title="Shaun T",
         url="plugin://plugin.video.youtube/user/"+YOUTUBE_CHANNEL_ID_14+"/",
         thumbnail="http://icons.iconarchive.com/icons/icons8/ios7/128/Sports-Exercise-icon.png",
         folder=True ) 
